4d798fa7a6f7f678128bf5bc4dc66cce5f38f5bc
Wojciech Dudzik <wojtekdudzik@o2.pl>
2025-08-12 10:10:06 +0200

    Dekoratory parametry

--- Day2/decorators.py
+++ Day2/decorators.py
@@ -37,10 +37,77 @@ if __name__ == '__main__':
 
     # Stwórz funkcję której zadaniem będzie poczekanie 3 sekundy i wypisanie na konsoli komunikatu.
     # Dodaj dekorator który zliczy czas wykonywania tej funkcji. Pobranie aktualnego czasu to: "time.time()" lub  datetime.now()
+    def licz_czas(fun):
+        def wew():
+            poczatek = datetime.now()
+            fun()
+            koniec = datetime.now()
+            print(f'Wywolanie trwalo {koniec - poczatek}')
 
+        return wew
 
-
-
+    @licz_czas
     def opakuj_mnie():
-        time.sleep(3)
+        #time.sleep(3)
         print("Robie ciekawe rzeczy w Pythonie")
+
+    opakuj_mnie()
+
+    # opakuj_mnie = dekorator(opakuj_mnie)
+    # opakuj_mnie()
+    # help(opakuj_mnie)
+
+    @licz_czas
+    def opakowanie_inne():
+        print("inna funkcja")
+
+
+    def dekorator_z_1_parametrem(fun):
+        def wew(liczba: int):
+            print("Hurra działa z parametrem")
+            print(fun(liczba))
+
+        return wew
+
+
+    @dekorator_z_1_parametrem
+    def dodaj_cztery(liczba: int) -> int:
+        return liczba + 4
+
+    dodaj_cztery(4)
+
+
+    def dekorator_do_funkcji_z_parameterami(fun):
+        def wew(*args, **kwargs):
+            print("Hurra działa zawsze")
+            fun(*args, **kwargs)
+            print("Po wszystkim")
+
+        return wew
+
+
+    @dekorator_do_funkcji_z_parameterami
+    def dekorowana(x: str):
+        print(f'siema {x}')
+
+    @dekorator_do_funkcji_z_parameterami
+    def dekorowana_bez_p():
+        print(f'siema')
+
+    dekorowana("Janek")
+    dekorowana(x="Wojtek")
+    dekorowana_bez_p()
+
+
+    @dekorator_do_funkcji_z_parameterami
+    def moja_suma(*liczby: tuple[int, ...]) -> int:
+        suma = 0
+        for i in liczby:
+            suma += i
+        print(f'Wynik to  {suma}')
+        return suma
+
+    moja_suma(1,2,3,3,4,5)
+
+    
+    # Napisz funkcje która przed i po wykonaniu innej funkcji wypisze 25 '*'     print("*" * 25)
---
+++ Day2/decorators_homework.py
@@ -0,0 +1,10 @@
+##############################################
+# ZADANIE 1
+# Napisz dekorator `log_call`, który będzie wypisywał w konsoli:
+# - nazwę funkcji
+# - przekazane argumenty
+# - wynik działania funkcji
+# Użyj go do udekorowania funkcji `add(a, b)`.
+##############################################
+
+
