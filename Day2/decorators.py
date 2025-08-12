c4867865bf6552cd9b01ee80eb11a65b4b8aab2e
Wojciech Dudzik <wojtekdudzik@o2.pl>
2025-08-12 09:46:44 +0200

    Dekoratory zadanie

--- Day2/decorators.py
+++ Day2/decorators.py
@@ -23,5 +23,24 @@ def dekorator(funkcje: Callable) -> Callable:
 
     return opakowujaca
 
+@dekorator
+def do_opakwoanie_2():
+    print("Skladnia dekorator pythona")
+
 if __name__ == '__main__':
     # https://refactoring.guru/pl/design-patterns/decorator
+
+    udekorwana = dekorator(do_opakowania)
+    udekorwana()
+
+    do_opakwoanie_2()
+
+    # Stwórz funkcję której zadaniem będzie poczekanie 3 sekundy i wypisanie na konsoli komunikatu.
+    # Dodaj dekorator który zliczy czas wykonywania tej funkcji. Pobranie aktualnego czasu to: "time.time()" lub  datetime.now()
+
+
+
+
+    def opakuj_mnie():
+        time.sleep(3)
+        print("Robie ciekawe rzeczy w Pythonie")
